95dd20ee2a467385818c082d349473c8340a9e97
ShantiRoseCodes <87835294+ShantiRoseCodes@users.noreply.github.com>
2021-10-20 19:09:22 +0200

    create Menu form added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App (){
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="createMenu">
+      <form>
+        <label for="category"> Category </label>
+          <select id="category" name="category">
+            <option value="mdEntree"> MENU DIA Entree </option> 
+            <option  value="mdMain"> MENU DIA Main </option> 
+            <option  value="mdDessert"> MENU DIA Dessert </option> 
+            <option  value="tapas"> Tapas </option> 
+            <option  value="entree"> Entree </option> 
+            <option  value="main"> Main </option> 
+            <option  value="dessert"> Dessert </option> 
+            <option  value="alcoholic"> Alcoholic </option> 
+            <option  value="nonalcoholic"> Nonalcoholic </option>
+          </select> 
+          <br></br>
+        <label> Dish / Drink
+          <input type="text"/>
+        </label>
+        <br></br>
+        <label> Details
+          <textarea name="details"></textarea>
+        </label>
+        <br></br>
+        <label> Image File
+          <input type="text"/>
+        </label>
+        <br></br>
+        <label> Price
+          <input type="number"/>
+        </label>
+        <br></br>
+        <button type = "submit">SUMBIT</button>
+      </form>
     </div>
   );
+
 }
 
-export default App;
+  
+
+
