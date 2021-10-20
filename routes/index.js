a8858167a8c291dbf0036d123cbddfc1a65eca89
ShantiRoseCodes <87835294+ShantiRoseCodes@users.noreply.github.com>
2021-10-20 16:57:35 +0200

    post method to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../model/helper");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send({ title: 'Express' });
 });
 
-router.post('/admin', function(req,res,next){
-  db()
-}
+router.post('/', async function(req,res,next){
+  let { category, dish, details, imagefile, price } = req.body;
 
-)
+  try{
+    let sql = 
+      `INSERT INTO menu (category, dish, details, imagefile, price)  
+      VALUES ("${category}", "${dish}", "${details}", "${imagefile}", "${price}");`;
+    await db(sql);
+    let results = db("SELECT * FROM menu;");
+    res.send(results.data);
+  } catch(err) {
+    res.status(500).send({ error: err.message});
+  }  
+});
 
 module.exports = router;
