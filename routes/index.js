324f871b6f9afc78f681962804801dd390fdfb9c
ShantiRoseCodes <87835294+ShantiRoseCodes@users.noreply.github.com>
2021-10-20 17:47:40 +0200

    added get and post methods for server.

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,17 @@ var router = express.Router();
 const db = require("../model/helper");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send({ title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.send({ title: 'Express' });
+// });
+
+router.get('/', async function(req, res, next){
+  try{
+    let results = await db("SELECT * FROM menu ORDER BY category;");
+    res.send(results.data);
+  } catch(err) {
+    res.status(400).send({ error: "Sorry. We are encountering technical difficulties."});
+  }  
 });
 
 router.post('/', async function(req,res,next){
@@ -13,10 +22,11 @@ router.post('/', async function(req,res,next){
   try{
     let sql = 
       `INSERT INTO menu (category, dish, details, imagefile, price)  
-      VALUES ("${category}", "${dish}", "${details}", "${imagefile}", "${price}");`;
+      VALUES ("${category}", "${dish}", "${details}", "${imagefile}", ${price});`;
     await db(sql);
-    let results = db("SELECT * FROM menu;");
+    let results = await db("SELECT * FROM menu;");
     res.send(results.data);
+    console.log(results.data);
   } catch(err) {
     res.status(500).send({ error: err.message});
   }  
